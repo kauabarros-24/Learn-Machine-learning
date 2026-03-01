4b979348f8c310f0ae542a6608c9026ff7d84bf5
user.name: kauabarros-25 <user.mail: martinsbarroskaua@gmail.com>
2026-03-01 17:53:57 -0300

    CHORE: Create simple ai model based in linear regression

--- src/best_country.py
+++ src/best_country.py
@@ -6,11 +6,9 @@ from sklearn.linear_model import LinearRegression
 data_root = "https://github.com/ageron/data/raw/main/"
 lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
 
-# colunas corretas do dataset
 X = lifesat[["GDP per capita"]].values
 Y = lifesat[["Life satisfaction"]].values
 
-# gráfico
 lifesat.plot(
     kind="scatter",
     grid=True,
@@ -19,10 +17,8 @@ lifesat.plot(
 )
 plt.show()
 
-# modelo
 model = LinearRegression()
 model.fit(X, Y)
 
-# previsão
 X_new = [[37655.2]]
 print(model.predict(X_new))
