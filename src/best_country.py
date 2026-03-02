c50ff5f0bb89e445753d71ad593cfd2ccc83c50a
user.name: kauabarros-25 <user.mail: martinsbarroskaua@gmail.com>
2026-03-02 13:38:40 -0300

    CHORE: Create a simple model to linear regression

--- src/best_country.py
+++ src/best_country.py
@@ -6,13 +6,13 @@ from sklearn.linear_model import LinearRegression
 data_root = "https://github.com/ageron/data/raw/main/"
 lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
 
-X = lifesat[["GDP per capita"]].values
+X = lifesat[["GDP per capita (USD)"]].values
 Y = lifesat[["Life satisfaction"]].values
 
 lifesat.plot(
     kind="scatter",
     grid=True,
-    x="GDP per capita",
+    x="GDP per capita (USD)",
     y="Life satisfaction"
 )
 plt.show()
